48b7af5446a202708de812c840b92c47f758b194
Mark McFate <summitt.dweller@gmail.com>
2021-03-28 12:01:51 -0500

    Scroll is not working

---
+++ static/js/checkTweetDate.js
@@ -0,0 +1,22 @@
+function checkTweetDate( ) {
+  // var card = document.getElementsByClassName('timeline-card')[0];
+  // var iframe = document.getElementById("twitter-widget-0");
+  // //console.log(iframe);
+  // var metadata = iframe.contentWindow.document.getElementsByClassName('timeline-Tweet-metadata')[0];
+  // //console.log(metadata);
+  // var target = metadata.getElementsByTagName('time')[0];
+  // //console.log(target);
+  // var pubTime = Date.parse(target.getAttribute('datetime'));
+  // //console.log(pubTime);
+  // 
+  // var now = Date.parse(Date());
+  // //console.log(now);
+  // 
+  // var diff = now - pubTime;
+  // 
+  // // check if tweet is more than 12 hours old
+  // if (diff > 12*60*60*1000) {
+  //   //alert("Tweet is stale");
+  //   card.style.display = "none";
+  // }
+}
